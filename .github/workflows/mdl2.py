d9c6acc63bad842f040445d6ba1c75904e408ec9
python-2024.12.9 <joelliu848@gmail.com>
2024-12-09 13:51:26 +0800

    python 2024.12.9

--- .github/workflows/mdl2.py
+++ .github/workflows/mdl2.py
@@ -1,9 +1,14 @@
 import cal2 as cal
-yr1 = int(input("請輸入年份: "))
-mn1 = int(input("請輸入月份: "))
-if 1 <= mn1 <= 12:
-    cal.prtcal(yr1,mn1)
-    #cal = calendar.TextCalendar()
-    #print(cal.formatmonth(year, month))
-else:
-    print("月份必須在1到12之間")
+
+counter=0
+while (counter<=0):
+    print('=' * 10,"請輸入要顯示該月月曆的年月",'=' * 10)
+    yr1 = int(input("請輸入年份: "))
+    mn1 = int(input("請輸入月份: "))
+    if 1 <= mn1 <= 12:
+        cal.prtcal(yr1,mn1)
+        counter=counter+1
+        #cal = calendar.TextCalendar()
+        #print(cal.formatmonth(year, month))
+    else:
+        print("月份必須在1到12之間，請重新輸入")
